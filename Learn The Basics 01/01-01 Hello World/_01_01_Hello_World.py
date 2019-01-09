17f29c0f86a6b4873ede9090dfcd03789a94ece7
Jorge Cordoba <jcordoba@outlook.com>
2019-01-08 22:22:31 -0600

    1st you stage them, then commit, then sync

---
+++ Learn The Basics 01/01-01 Hello World/_01_01_Hello_World.py
@@ -0,0 +1,14 @@
+print ("sup world!")
+print("look at me! I'm python master")
+
+x=1
+if x==1:
+    print("\
+i'm 1 boach \
+a multi line one ma'bitch")
+
+if 1==2:
+    print("ok, I'm not one, I'm a two \
+    and because of that i'm not going to be displayed")
+        
+print("eof")
---
+++ Learn The Basics 01/01-02 Variables and Types/_01_02_Variables_and_Types.py
@@ -0,0 +1,20 @@
+someint = 666
+print(someint)
+
+print("the same [666] but now as a float [{}]".format(float(someint)))
+
+someint=6.666
+print("the same [666] but now as a float reinstantiated [{}]".format(float(someint)))
+
+
+mastring = 'ma\'string'
+print(mastring)
+
+one=1
+two=2
+three=one+two
+print (three)
+
+a, b = 3, 4
+print(a, b, 'witchcraft!!!')
+
