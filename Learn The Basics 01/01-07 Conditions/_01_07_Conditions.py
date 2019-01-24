78a2b9301ccdf6e1b8d3d6bc49040c93be9e5af1
Jorge Cordoba <jcordoba@outlook.com>
2019-01-23 22:11:26 -0600

    Conditions

---
+++ Learn The Basics 01/01-07 Conditions/_01_07_Conditions.py
@@ -0,0 +1,23 @@
+name = "cosa"
+if (name=="loca"):
+    print("viviendo")
+    print("la vida")
+    print("loca")
+elif(name=="kokin"):
+    print("esto")
+    print("no deberia")
+    print("pasar")
+print("eof")
+
+
+x=[1,2,3]
+y=[1,2,3]
+if(x==y):
+    print(" == ")
+elif(x is y):
+    print(" x is y ")
+elif(x is x):
+    print(" x is x ")
+
+if(x is x):
+    print("i said x is x")
